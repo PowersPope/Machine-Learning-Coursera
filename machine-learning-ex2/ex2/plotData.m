977054b7f29aa6bc82922aa4d8335ff42d60c0cf
Andrew Powers <andrewpowers@MacBook-Pro.local>
2021-06-11 11:13:25 -0700

    Added folders machine-learning-ex1 to 3, some are not complete they were messed up when I moved them from my other computer.

---
+++ machine-learning-ex2/ex2/plotData.m
@@ -0,0 +1,36 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+
+%Indices for the pos and negative examples in the training set
+pos = find(y==1); neg = find(y==0);
+
+%Plot the neg and pos examples on a graph, using + and o to distinguish them
+plot(X(pos,1), X(pos, 2), 'k+', 'LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg,1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
+legend({'Admitted', 'Not Admitted'}, 'location', 'northeast');
+xlabel('Exam 1 score');
+ylabel('Exam 2 score');
+
+
+
+
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
